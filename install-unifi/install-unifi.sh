2f0e5484559b297e7f5e8f332f0f8290b7178b4a
John Burwell <john@gozoinks.com>
2013-10-11 23:19:27 -0500

    More user feedback

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -5,48 +5,61 @@
 
 # Add the fstab entries apparently required for OpenJDK 6:
 if [ $(grep -c fdesc /etc/fstab) -eq 0 ]; then
-  echo "Adding fdesc filesystem to /etc/fstab..."
+  echo -n "Adding fdesc filesystem to /etc/fstab..."
   echo -e "fdesc\t\t\t/dev/fd\t\tfdescfs\trw\t\t0\t0" >> /etc/fstab
+  echo "done."
 fi
 
 if [ $(grep -c proc /etc/fstab) -eq 0 ]; then
-  echo "Adding procfs filesystem to /etc/fstab..."
+  echo -n "Adding procfs filesystem to /etc/fstab..."
   echo -e "proc\t\t\t/proc\t\tprocfs\trw\t\t0\t0" >> /etc/fstab
+  echo "done."
 fi
 
 # Run mount to mount the two new filesystems:
-echo "Mounting new filesystems."
+echo -n "Mounting new filesystems..."
 /sbin/mount -a
+echo " done."
 
 # Install mongodb, OpenJDK 6, and unzip (required to unpack Ubiquiti's download):
 # -F skips a package if it's already installed, without throwing an error.
-/usr/sbin/pkg_add -F -r mongodb openjdk6 unzip
+echo -n "Installing required packages..."
+/usr/sbin/pkg_add -vFr mongodb openjdk6 unzip
+echo " done."
 
 # Switch to a temp directory for the Unifi download:
 cd `mktemp -d -t unifi`
 
 # Download the controller from Ubiquiti (assuming acceptance of the EULA):
-echo "Downloading the UniFi controller software..."
+echo -n "Downloading the UniFi controller software..."
 /usr/bin/fetch http://dl.ubnt.com/unifi/2.4.5/UniFi.unix.zip
+echo " done."
 
 # Unpack the archive into the /usr/local directory:
-echo "Installing UniFi controller in /usr/local..."
+echo -n "Installing UniFi controller in /usr/local..."
 /usr/local/bin/unzip UniFi.unix.zip -d /usr/local
+echo " done."
 
 # Update Unifi's symbolic link for mongod to point to the version we just installed:
-echo "Updating mongod link..."
+echo -n "Updating mongod link..."
 /bin/ln -sf /usr/local/bin/mongod /usr/local/UniFi/bin/mongod
+echo " done."
 
 # Fetch the rc script from github:
+echo -n "Installing rc script..."
 /usr/bin/fetch -o /usr/local/etc/rc.d/unifi https://raw.github.com/gozoinks/unifi-pfsense/master/rc.d/unifi
+echo " done."
 
 # Fix permissions so it'll run
 chmod +x /usr/local/etc/rc.d/unifi
 
 # Add the startup variable to rc.conf.local.
 # Eventually, this step will need to be folded into pfSense, which manages the main rc.conf.
-echo "Enabling the unifi service."
+echo -n "Enabling the unifi service..."
 echo "unifi_enable=YES" >> /etc/rc.conf.local
+echo " done."
 
 # Start it up:
+echo -n "Starting the unifi service..."
 /usr/sbin/service unifi start
+echo " done."
