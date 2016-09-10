b2485741dcaa3dd103141b1df16f53e96b3ef615
jmcphail <james@isti.ca>
2016-09-10 13:12:21 -0700

    Update Unifi Version

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -7,7 +7,7 @@
 OS_ARCH=`getconf LONG_BIT`
 
 # The latest version of UniFi:
-UNIFI_SOFTWARE_URL="https://dl.ubnt.com/unifi/5.0.6/UniFi.unix.zip"
+UNIFI_SOFTWARE_URL="https://dl.ubnt.com/unifi/5.0.7/UniFi.unix.zip"
 
 # The rc script associated with this branch or fork:
 RC_SCRIPT_URL="https://raw.githubusercontent.com/gozoinks/unifi-pfsense/master/rc.d/unifi.sh"
