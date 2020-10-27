6d397ad33403933cbdc5e82509ae869405fa84af
Genevieve Kidwell <71857593+gnkidwell@users.noreply.github.com>
2020-10-27 13:29:07 -0500

    Bumps version to 5.14.23 Stable

--- install-unifi-stable 5.14.23/install-unifi 5.14.23.sh
+++ install-unifi-stable 5.14.23/install-unifi 5.14.23.sh
@@ -7,7 +7,7 @@
 UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/5.14.23/UniFi.unix.zip"
 
 # The rc script associated with this branch or fork:
-RC_SCRIPT_URL="https://raw.githubusercontent.com/gozoinks/unifi-pfsense/master/rc.d/unifi.sh"
+RC_SCRIPT_URL="https://raw.githubusercontent.com/gnkidwell/unifi-pfsense/beta/rc.d/unifi.sh"
 
 # If pkg-ng is not yet installed, bootstrap it:
 if ! /usr/sbin/pkg -N 2> /dev/null; then
