f544ff4bda3a4b8b9d81d810c65869a677db80a3
ReinaldoFeitosa <reinaldo@mbcsoftware.com.br>
2023-03-15 12:54:27 -0300

    Update UniFi to 7.3.83

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -4,7 +4,7 @@
 # Installs the Uni-Fi controller software on a FreeBSD machine (presumably running pfSense).
 
 # The latest version of UniFi:
-UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/7.2.92/UniFi.unix.zip"
+UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/7.3.83/UniFi.unix.zip"
 
 
 # The rc script associated with this branch or fork:
