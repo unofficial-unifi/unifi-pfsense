429635cfcd980c11c53d9586f94bd4bf4505b6dc
tong2x <40678733+tong2x@users.noreply.github.com>
2023-07-23 15:10:24 +0800

    Update install-unifi.sh

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -12,7 +12,7 @@ RC_SCRIPT_URL="https://raw.githubusercontent.com/unofficial-unifi/unifi-pfsense/
 
 # List of valid/supported mongodb package names, sorted with the latest being first
 # As UniFi adds support for new mongodb versions, just prepend them to this list
-SUPPORTED_MONGODB_PACKAGES="mongodb42 mongodb40 mongodb36 mongodb34 mongodb32 mongodb"
+SUPPORTED_MONGODB_PACKAGES="mongodb44 mongodb42 mongodb40 mongodb36 mongodb34 mongodb32 mongodb"
 
 # If pkg-ng is not yet installed, bootstrap it:
 if ! /usr/sbin/pkg -N 2> /dev/null; then
