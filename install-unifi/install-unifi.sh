c2ff0478e8dc3a1d20fbb5311b8017226798b9a9
tong2x <40678733+tong2x@users.noreply.github.com>
2025-10-25 08:49:05 +0800

    10.0.140
    10.0.140

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -4,7 +4,7 @@
 # Installs the Uni-Fi controller software on a FreeBSD machine (presumably running pfSense).
 
 # The latest version of UniFi:
-UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/9.5.21-6nxxr6v29z/UniFi.unix.zip"
+UNIFI_SOFTWARE_URL="https://dl.ui.com/unifi/10.0.140-26hz11fge3/UniFi.unix.zip"
 
 
 # The rc script associated with this branch or fork:
